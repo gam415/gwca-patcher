3096c20a8a3ccbc285fa892b21b7ffcb966e0e72
Mike Jerred <mjerred.work@gmail.com>
2026-04-01 00:18:31 +0100

    fix: update for latest toolbox

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,34 @@
-use pelite::pe32::{Pe, PeFile};
 use std::{fs, io::Write};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let path = "gwca.dll";
 
   let mut data = fs::read(path)?;
-  let pe = PeFile::from_bytes(&data)?;
-  let exports = pe.exports()?.by()?;
-  let names = exports.names();
-  for i in 0..names.len() {
-    if let Ok(name) = pe.derva_c_str(names[i]) && name.to_string().contains("GetIsAgentTargettable") {
-      let index = exports.name_indices()[i];
-      let rva = exports.functions()[index as usize];
-      let file_offset = pe.rva_to_file_offset(rva).expect("RVA not mapped");
 
-      const SCAN_CODE_LENGTH: usize = 8;
-      let scan_code: [u8; SCAN_CODE_LENGTH] = [0xf7, 0x40, 0x10, 0x00, 0x00, 0x01, 0x00, 0x74];
-
-      for i in file_offset..file_offset+1000 {
-        let bytes = &data[i..i+SCAN_CODE_LENGTH];
-        if bytes == scan_code {
-          data[i+7] = 0xEB; // change JZ to JMP
-
-          fs::OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .open(path)?
-            .write_all(&data)?;
-
-          return Ok(())
-        }
+  const SCAN_CODE_LENGTH: usize = 8;
+  let scan_code: [u8; SCAN_CODE_LENGTH] = [0xf7, 0x40, 0x10, 0x00, 0x00, 0x01, 0x00, 0x74];
+  const TOTAL_COUNT: usize = 3;
+  let mut count = 0;
+  for i in 0..(data.len() - SCAN_CODE_LENGTH) {
+    let bytes = &data[i..i+SCAN_CODE_LENGTH];
+    if bytes == scan_code {
+      data[i+7] = 0xEB; // change JZ to JMP
+      count = count + 1;
+      if count >= TOTAL_COUNT {
+        break;
       }
-
-      return Err("Could not find the place to modify in the GetIsAgentTargettable function".into());
     }
   }
 
-  Err("Could not find exported function GetIsAgentTargettable".into())
+  if count < TOTAL_COUNT {
+    return Err(format!("Only found {count}/{TOTAL_COUNT} instances of minipet target checks").into());
+  }
+
+  fs::OpenOptions::new()
+    .write(true)
+    .truncate(true)
+    .open(path)?
+    .write_all(&data)?;
+
+  return Ok(());
 }
